729476300182852f33f61f343e7a485bfbe0c861
HasanChase <70952852+HasanChase@users.noreply.github.com>
2021-12-26 20:51:05 +0100

    Add homepage

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="assets/CSS/index.css" type="text/css">
+    </head>
+    <body class="page-structuur">
+        <div>
+            <img src="assets/images/Dumbledore.png" alt="Dumbledore" class="homepage-Add-People-image">
+        </div>
+        <div>  
+            <button class="buttons" onclick="window.location.href='addPerson.php';">
+                Add person
+            </button>
+        </div>
+        <div id="padding-tussen-btn-img">
+            <img src="assets/images/Characters.jpg" alt="Characters_In_The_Harry_Potter_Movie" class="homepage-Put-in-a-house-image">
+        </div>
+        <div>
+            <button class="buttons" onclick="window.location.href='peopleInHouse.php';">
+                Put people in a house
+            </button>
+        </div>
+    </body>
+</html>
