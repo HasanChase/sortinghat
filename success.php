acf2a3145352877ed513bed5bb62faad69a15e69
HasanChase <70952852+HasanChase@users.noreply.github.com>
2021-12-26 20:58:11 +0100

    Add success page

---
+++ success.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="assets/CSS/index.css" type="text/css">
+    </head>
+    <body class="page-structuur">
+        <h1>
+            congratulations the profile is added to our database!
+        </h1>
+        <button class="buttons" onclick="window.location.href='peopleInHouse.php';">
+                Put people in a house
+        </button>
+    </body>
+</html>
