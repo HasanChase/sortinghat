48600c2f6a74cec0159fb04f120fa0644cd195ff
HasanChase <70952852+HasanChase@users.noreply.github.com>
2021-12-26 21:02:21 +0100

    Add end page

---
+++ end.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="assets/CSS/index.css" type="text/css">
+    </head>
+    <body class="page-structuur">
+        <div>
+            <div>
+                <img src="assets/images/end_image.png" alt="Character_In_The_Harry_potter_movie" class="endpage-image">
+            </div>
+            <h1>
+                That's it, You rated them all!
+            </h1>
+        </div>
+    </body>
+</html>
